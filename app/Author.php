d9c08534220fcc4baebc97762c52a831bc55e218
Jeff Perkinson <jmperkinson@gmail.com>
2020-06-19 17:13:49 -0400

    Define fillable model fields.

--- app/Author.php
+++ app/Author.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     protected $table = 'authors';
+    protected $fillable = [     // fields that are allowed mass assignment
+        'name',
+        'title',
+        'company',
+        'email'
+    ];
     
     public function books() 
     {   // define the relationship between authors and books; an author has many books
--- app/Book.php
+++ app/Book.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $table = 'books';
+    protected $fillable = [
+        'title',
+        'author_id',
+        'abstract'
+    ];
 
     public function author() 
     {   // define the relationship between books an authors.  Books belong to authors

7d96a7012d2d159a86b904acf14e733e92e017c6
Jeff Perkinson <jmperkinson@gmail.com>
2020-06-19 17:02:51 -0400

    Define author and book model relationships.

--- app/Author.php
+++ app/Author.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    protected $table = 'authors';
+    
+    public function books() 
+    {   // define the relationship between authors and books; an author has many books
+        return $this->hasMany(Book::class);
+    }
 }
--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $table = 'books';
+
+    public function author() 
+    {   // define the relationship between books an authors.  Books belong to authors
+        // its an inverse of the hasMany relationship defined in Author.php
+        return $this->belongsTo(Author::class);
+    }
 }

51affd2f4e33b0a125f3029a7656f1081315e812
Jeff Perkinson <jmperkinson@gmail.com>
2020-06-19 16:18:38 -0400

    Add Author and Book models and home page links.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Build A Laravel REST API</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,18 +81,25 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Build a Laravel REST API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="../README.md">README</a>
+                    <a href="{{env('app_url')}}/api/authors/" target="_blank">Authors</a>
+                    <a href="{{env('app_url')}}/api/books/" target="_blank">Books</a>
+                    <!-- JSON formatter -->
+                    <a href="https://jsonformatter.curiousconcept.com/"
+                    target="_blank">JSON Formatter</a>
+                    <!-- default laravel links -->
+                    <!-- <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
                 </div>
             </div>
         </div>
